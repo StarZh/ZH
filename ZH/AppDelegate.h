fa1f5d69698e2e7a1306de35073daf33984f2b2c
honghong <honghong>
2015-09-07 12:23:55 +0800

    add project

---
+++ ZH/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZH
+//
+//  Created by honghong on 15/9/7.
+//  Copyright (c) 2015年 honghong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZH/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZH
+//
+//  Created by honghong on 15/9/7.
+//  Copyright (c) 2015年 honghong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
